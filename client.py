f5f956f64b19cd9c2d5ffde1ce582e6228bd9d29
iLn0ur <slevin_@mail.ru>
2023-04-30 15:47:22 +0300

    add some func

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import time
 import json
-
-client = socket(AF_INET, SOCK_STREAM)
-client.connect(('localhost', 7777))
+import argparse
 
 
-def form_precense_msg():
+def form_precense():
     message = {
-        ACTION: PRESENCE,
-        TIME: time.time(),
-        USER: {
-            ACCOUNT_NAME: account_name
+        "action": "presense",
+        "time": time.time(),
+        "type": "status",
+        "user": {
+                "account_name": "account_name",
+                "status": "Yep, I am here!"
         }
     }
     json_msg = json.dumps(message)
@@ -20,9 +20,111 @@ def form_precense_msg():
     return binary_msg
 
 
-client.send(form_precense_msg())
+def form_probe():
+    message = {
+        "action": "probe",
+        "time": time.time(),
+        "user": {
+            "account": "account_name"
+        }
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
 
-tm = client.recv(1024)
-client.close()
+    return binary_msg
 
-print("Текущее время: %s" % tm.decode('ascii'))
+
+def form_msg():
+    message = {
+        "action": "msg",
+        "time": time.time(),
+        "to": "account_name",
+        "from": "account_name",
+        "encoding": "ascii",
+        "message": "message"
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
+
+    return binary_msg
+
+
+def form_quit():
+    message = {
+        "action": "quit",
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
+
+    return binary_msg
+
+
+def form_authenticate():
+    message = {
+        "action": "authenticate",
+        "time": time.time(),
+        "user": {
+                "account_name": "C0deMaver1ck",
+                "password": "CorrectHorseBatterStaple"
+            }
+
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
+
+    return binary_msg
+
+
+def form_join():
+    message = {
+        "action": "join",
+        "time": time.time(),
+        "room": "#room_name"
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
+
+    return binary_msg
+
+
+def form_leave():
+    message = {
+        "action": "leave",
+        "time": time.time(),
+        "room": "#room_name"
+    }
+    json_msg = json.dumps(message)
+    binary_msg = json_msg.encode('ascii')
+
+    return binary_msg
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='start script args')
+    parser.add_argument(
+        '-addr',
+        type=str,
+        default='localhost',
+        help='addr of server, default localhost'
+    )
+    parser.add_argument(
+        '-p',
+        type=int,
+        default=7777,
+        help='port number, default 7777'
+    )
+
+    args = parser.parse_args()
+
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect((args.addr, args.p))
+
+    client.send(form_precense())
+
+    tm = client.recv(1024)
+    client.close()
+    tm_str = tm.decode('utf-8')
+    tm_json = json.loads(tm_str)
+
+    print("Ответ сервера: %s" % tm_json["response"])
--- server.py
+++ server.py
@@ -1,20 +1,62 @@
+import json
 from socket import socket, AF_INET, SOCK_STREAM
 import time
+import argparse
 
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(('localhost', 7777))
-s.listen(5)
+def main(addr, p):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((addr, p))
+    s.listen(5)
 
-while True:
-    client, addr = s.accept()
-    print("Получен запрос на соединение от %s" % str(addr))
-    timestr = time.ctime(time.time()) + "\n"
+    while True:
+        client, addr = s.accept()
+        print("Получен запрос на соединение от %s" % str(addr))
+        timestr = time.ctime(time.time()) + "\n"
 
-    binary_msg = client.recv(1024)
-    json_msg = binary_msg.decode('ascii')
+        binary_msg = client.recv(1024)
+        client_msg = binary_msg.decode('ascii')
+        json_msg = json.loads(client_msg)
+        print(json_msg["action"])
 
-    print(json_msg)
+        server_json = {
+                        "response": 200,
+                        "alert":"Необязательное сообщение/уведомление"
+        }
 
-    client.send(timestr.encode('ascii'))
-    client.close()
+        server_str = json.dumps(server_json)
+        server_binary = server_str.encode('utf-8')
+        client.send(server_binary)
+        client.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='start script args')
+    parser.add_argument(
+        '-addr',
+        type=str,
+        default='localhost',
+        help='addr of server, default localhost'
+    )
+    parser.add_argument(
+        '-p',
+        type=int,
+        default=7777,
+        help='port number, default 7777'
+    )
+    parser = argparse.ArgumentParser(description='start script args')
+    parser.add_argument(
+        '-addr',
+        type=str,
+        default='localhost',
+        help='addr of server, default localhost'
+    )
+    parser.add_argument(
+        '-p',
+        type=int,
+        default=7777,
+        help='port number, default 7777'
+    )
+
+    args = parser.parse_args()
+    main(args.addr, args.p)
