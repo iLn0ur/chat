b3bad414ba28ea65664e5257c6ee167bb052cbf1
iLn0ur <slevin_@mail.ru>
2023-05-04 16:03:12 +0300

    add logging

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import json
+import logging
+import sys
 from socket import socket, AF_INET, SOCK_STREAM
 # import time
 from client import get_params
+import log.server_config_log
+
+logger = logging.getLogger('server_log')
 
 
 def client_msg_receive(binary_msg):
     client_msg = binary_msg.decode('ascii')
     json_msg = json.loads(client_msg)
+    logger.debug('получено сообщение от клиента')
     return json_msg
 
 
@@ -18,20 +24,29 @@ def server_resp():
 
     server_str = json.dumps(server_json)
     server_binary = server_str.encode('utf-8')
+    logger.debug('отправлено сообщение клиенту')
     return server_binary
 
 
 def main(addr, p):
+
+    if not 1023 < p < 65536:
+        logger.critical(f'Попытка запуска сервера с указанием неподходящего порта {p}')
+        sys.exit()
+
+    logger.info(f'Запущен сервер, порт для подключений: {p}, '
+                f'адрес с которого принимаются подключения: {addr}. '
+                f'Если адрес не указан, принимаются соединения с любых адресов.')
     s = socket(AF_INET, SOCK_STREAM)
     s.bind((addr, p))
     s.listen(5)
 
     while True:
         client, addr = s.accept()
-        print(f"Получен запрос на соединение от {str(addr)}")
+        logger.info(f"Получен запрос на соединение от {str(addr)}")
         # timestr = time.ctime(time.time()) + "\n"
         binary_msg = client.recv(1024)
-        print(client_msg_receive(binary_msg)["action"])
+        logger.info(client_msg_receive(binary_msg)["action"])
 
         client.send(server_resp())
         client.close()
